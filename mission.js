3a9fb34f6ca4e824226a80e9fed7f181bc05c1c2
mozzynthengu <mosiah.nthengu@gmail.com>
2025-10-02 22:43:44 -0600

    Update mission.js

--- mission.js
+++ mission.js
@@ -5,11 +5,12 @@ const logo = document.querySelector('footer img');
 function changeTheme() {
   if (themeSelector.value === 'dark') {
     body.classList.add('dark');
-    logo.src = 'images/byui-logo_white.png'; // White logo for dark theme
+    logo.src = 'byui-logo_white.png'; // White logo for dark theme
   } else {
     body.classList.remove('dark');
-    logo.src = 'images/logo.jpg'; // Blue logo for light theme
+    logo.src = 'logo.jpg'; // Blue logo for light theme
   }
 }
 
 themeSelector.addEventListener('change', changeTheme);
+
